629cffeba7ae9d36ae2347fe088ff3c7ba57ca73
YaCpotato <yasshisshy@gmail.com>
2020-03-01 21:55:24 +0900

    fix task add function

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -36,14 +36,54 @@ class NewsController extends Controller
      */
     public function store(Request $request)
     {
+        //env('SLACK_WEBHOOK_URL')
         $project = new Todo;
         $project->tile = $request->title;
         $project->description = $request->description;
         $project->state = $request->status;
 
         $project->save();
+        $message = array(
+            'username' => 'Bot',
+            'text' => $request->title.'が追加されました',
+            );
+        $this->send_to_slack($message);
         return redirect('/news');
     }
+/**
+ * 
+ * $message = array(
+ *       'username' => 'Bot',
+ *       'text' => 'fooooo!!!',
+ *       );
+ */
+    private function send_to_slack($message) 
+    {
+        $webhook_url = env('SLACK_WEBHOOK_URL');
+        // $options = array(
+        //     'http' => array(
+        //     'method' => 'POST',
+        //     'header' => 'Content-Type: application/json',
+        //     'content' => json_encode($message),
+        //     )
+        // );
+        $msg = json_encode($message);
+        $msg = 'payload=' . urlencode($msg);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $webhook_url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $msg);
+        curl_exec($ch);
+        curl_close($ch);
+        //$response = file_get_contents($webhook_url, false, stream_context_create($options));
+        //exit(var_dump($response));
+        return;    
+    }
+
+
 
     /**
      * Display the specified resource.
